98b8e516139efe031674f4bfd968f6587cd84c75
bedine <bedine@DESKTOP-009EGNO>
2017-03-14 19:51:26 -0300

    init

---
+++ bedine.licenca/bedine.licenca/TipodeLicenca.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bedine.licenca
+{
+    public class TipodeLicenca
+    {
+        public string TRIAL { get; set; }
+    }
+}
